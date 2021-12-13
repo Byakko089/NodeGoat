5740b6eacd303326ec447be960158b2ca079badc
Byakko089 <96025767+Byakko089@users.noreply.github.com>
2021-12-13 20:54:53 -0300

    FIX A10

--- app/routes/index.js
+++ app/routes/index.js
@@ -73,8 +73,9 @@ const index = (app, db) => {
     app.get("/memos", isLoggedIn, memosHandler.displayMemos);
     app.post("/memos", isLoggedIn, memosHandler.addMemos);
 
-    // Handle redirect for learning resources link
-    app.get("/learn", isLoggedIn, (req, res) => {
+    // Handle redirect for learning resources link (FIX A10)
+      app.get("/learn", function (req, res, next) {
+    //app.get("/learn", isLoggedIn, (req, res) => {
         // Insecure way to handle redirects by taking redirect url from query string
         return res.redirect(req.query.url);
     });
